1aa7923aefa9c040bbcc3666dcc8de2c2cd6d7c6
Fernando1Borges <centraloro@gmail.com>
2017-01-02 17:13:33 -0600

    Add other solution

--- 5kyu/DidIFinishMySudoku.js
+++ 5kyu/DidIFinishMySudoku.js
@@ -136,3 +136,26 @@ function check(arr) {
 //                         [2, 8, 7, 4, 1, 9, 6, 3, 5],
 //                         [3, 4, 5, 2, 8, 6, 1, 7, 9]])
 // should return "Finished!"
+
+// Other Solutions
+// Credit: Botanic, fs1923
+
+// function doneOrNot(board){
+//   var set1Array = new Set(),
+//       set2Array = new Set(),
+//       set3Array = new Set();
+//
+//   for (var i = 0; i < 9; i++) {
+//     for (var j = 0; j < 9; j++){
+//       set1Array.add(board[i][j]);
+//       set2Array.add(board[j][i]);
+//       set3Array.add(board[(i%3)*3+j%3][Math.floor(i/3)*3+Math.floor(j/3)]);
+//     }
+//     if (set1Array.size != 9 || set2Array.size != 9 || set3Array.size != 9 )
+//       return "Try again!";
+//     set1Array.clear();
+//     set2Array.clear();
+//     set3Array.clear();
+//   }
+//   return "Finished!";
+// }
