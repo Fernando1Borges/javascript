86c3093656973af8b1bdbc29d3bc86b7390a6a41
Fernando1Borges <centraloro@gmail.com>
2017-01-04 08:45:28 -0600

    Add my first kata

---
+++ CreatedKatas/BuildingThePyramids.js
@@ -0,0 +1,31 @@
+// 7kyu Kata created by Konrad Gnat
+// 01/04/16
+
+// Initial solution
+// function pyramid(height, area) {
+//   // Your code here...
+// }
+
+// Complete Solution
+function pyramid(height, area) {
+    var str = "";
+    var base = area / height;
+    var count = 0;
+    while(count < (base * 2)) {
+      str += "X";
+      count++;
+    }
+    return str;
+}
+
+
+//Test cases
+
+//
+// describe("pyramid", function(){
+//   it("should work for some examples", function(){
+//     Test.assertEquals(pyramid(8, 48), 'XXXXXXXXXXXX');
+//     Test.assertEquals(pyramid(22, 220), 'XXXXXXXXXXXXXXXXXXXX');
+//     Test.assertEquals(pyramid(14, 112), 'XXXXXXXXXXXXXXXX');
+//   });
+// });
