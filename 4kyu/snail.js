621ed4e96f366af9449ceb12afb5931a56e181b5
Fernando1Borges <centraloro@gmail.com>
2017-01-13 09:52:27 -0600

    Add other solutions to snail

--- 4kyu/snail.js
+++ 4kyu/snail.js
@@ -44,4 +44,25 @@ snail([ [ 1, 2, 3, 4, 5, 6 ],
 //   [ 18, 31, 36, 35, 26, 9 ],
 //   [ 17, 30, 29, 28, 27, 10 ],
 //   [ 16, 15, 14, 13, 12, 11 ] ]
-// ✘ snail([]) - Expected: '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]', instead got: '\'1,2,3,4,5,6, 7, 8, 9, 10, 11,12,13,14,15,16, 17,18,19,20\''
+// ✘ snail([]) - Expected: '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]',
+
+
+
+//OTHER SOLUTIONS
+// Credit: altrubypirate, xiaright, Unihedron, YGYOOO, link., theyellowkang (plus 4 more warriors)
+// snail = function(array) {
+//   var result;
+//   while (array.length) {
+//     // Steal the first row.
+//     result = (result ? result.concat(array.shift()) : array.shift());
+//     // Steal the right items.
+//     for (var i = 0; i < array.length; i++)
+//       result.push(array[i].pop());
+//     // Steal the bottom row.
+//     result = result.concat((array.pop() || []).reverse());
+//     // Steal the left items.
+//     for (var i = array.length - 1; i >= 0; i--)
+//       result.push(array[i].shift());
+//   }
+//   return result;
+// }
