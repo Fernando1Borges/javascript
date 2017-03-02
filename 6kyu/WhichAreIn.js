5915354b315330c7ecb188ed856710dfa41e4d87
konradgnat <konradmgnat@gmail.com>
2017-03-02 12:01:37 -0600

    Add other solutions to 6kyu kata

--- 6kyu/WhichAreIn.js
+++ 6kyu/WhichAreIn.js
@@ -28,3 +28,32 @@ a1 = ["live", "strong", "arp"]
 Test.assertSimilar(inArray(a1, a2), ["arp", "live", "strong"])
 a1 = ["tarp", "mice", "bull"]
 Test.assertSimilar(inArray(a1, a2), [])
+
+
+// OTHER SOLUTIONS
+//OverZealous, Meow, MaximillianVoss, ryng2
+
+function inArray(arr1, arr2) {
+  return arr1.filter(function(needle) {
+    return arr2.some(function(haystack) {
+      return haystack.indexOf(needle) > -1;
+    });
+  }).sort();
+}
+
+
+//st8998, rubbish, Nell
+
+function inArray(array1,array2){
+  return array1
+    .filter(a1 => array2.find(a2 => a2.match(a1)))
+    .sort()
+}
+
+
+//arnisritins
+
+function inArray(a1, a2) {
+  var str = a2.join(' ');
+  return a1.filter(s => str.indexOf(s) !== -1).sort();
+}
